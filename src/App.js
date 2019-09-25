a866490976a4cf4f75723766c4a8c6afdcb5074b
gps2601 <smith260194@gmail.com>
2019-09-25 22:34:45 +0100

    Seperating into own components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
 import React from 'react'
-import { vote, createAnecdote } from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = ({store}) => {
-  const anecdotes = store.getState()
-
-  const addAnecdote = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    store.dispatch(
-      createAnecdote(content)
-    )
-  }
 
   return (
     <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.sort(function(a, b) {
-        var one = a.votes
-        var two = b.votes
-        return one > two ? -1 : 1
-      }).map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => store.dispatch(vote(anecdote.id))}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div><input name='anecdote' /></div>
-        <button type='submit'>create</button>
-      </form>
+      <AnecdoteForm store={store} />
+      <AnecdoteList store={store} />
     </div>
   )
 }
